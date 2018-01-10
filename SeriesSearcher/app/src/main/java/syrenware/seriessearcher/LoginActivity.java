0faa60fda7a867d276a6f99f4cb7ba143a981cab
matthewsyren <matthew.syren@gmail.com>
2018-01-10 11:33:01 +0200

    Added forgot password functionality to the LoginActivity

--- SeriesSearcher/app/src/main/java/syrenware/seriessearcher/LoginActivity.java
+++ SeriesSearcher/app/src/main/java/syrenware/seriessearcher/LoginActivity.java
@@ -1,15 +1,21 @@
 package syrenware.seriessearcher;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.text.InputType;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.ProgressBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -28,7 +34,7 @@ public class LoginActivity extends AppCompatActivity {
             firebaseAuth = FirebaseAuth.getInstance();
 
             //Hides ProgressBar
-            toggleProgressBar(View.INVISIBLE);
+            toggleProgressBarVisibility(View.INVISIBLE);
 
             //Takes the user to the HomeActivity if they have already signed in
             User user = new User(this);
@@ -43,7 +49,7 @@ public class LoginActivity extends AppCompatActivity {
     }
 
     //Method toggles the visibility of the ProgressBar
-    public void toggleProgressBar(int visibility){
+    public void toggleProgressBarVisibility(int visibility){
         try{
             ProgressBar progressBar = findViewById(R.id.progress_bar);
             progressBar.setVisibility(visibility);
@@ -70,7 +76,7 @@ public class LoginActivity extends AppCompatActivity {
             final LoginActivity loginActivity = this;
 
             //Displays ProgressBar
-            toggleProgressBar(View.VISIBLE);
+            toggleProgressBarVisibility(View.VISIBLE);
 
             //Tries to sign the user in using the Firebase authentication database
             firebaseAuth.signInWithEmailAndPassword(user.getUserEmailAddress(), user.getUserPassword()).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
@@ -84,7 +90,7 @@ public class LoginActivity extends AppCompatActivity {
                     else{
                         Log.w("TAG", "signInWithEmail", task.getException());
                         Toast.makeText(LoginActivity.this, "Authentication failed.", Toast.LENGTH_SHORT).show();
-                        toggleProgressBar(View.INVISIBLE);
+                        toggleProgressBarVisibility(View.INVISIBLE);
                     }
                 }
             });
@@ -118,6 +124,86 @@ public class LoginActivity extends AppCompatActivity {
         try{
             Intent intent = new Intent(LoginActivity.this, CreateAccountActivity.class);
             startActivity(intent);
+        }
+        catch(Exception exc){
+            Toast.makeText(getApplicationContext(), exc.getMessage(), Toast.LENGTH_LONG).show();
+        }
+    }
+
+    //Method initiates the password recovery feature of this app
+    public void forgotPasswordOnClick(View view){
+        try{
+            displayInputMessage("Please enter your email address. An email with a link to reset your password will be sent to you.");
+        }
+        catch(Exception exc){
+            Toast.makeText(getApplicationContext(), exc.getMessage(), Toast.LENGTH_LONG).show();
+        }
+    }
+
+    //Method displays an AlertDialog to get an email address from the user
+    public void displayInputMessage(String message){
+        try{
+            //Creates AlertDialog content
+            AlertDialog alertDialog = new AlertDialog.Builder(this).create();
+            TextView textView = new TextView(this);
+            textView.setText(message);
+            textView.setTypeface(null, Typeface.BOLD);
+            final EditText input = new EditText(this);
+            input.setInputType(InputType.TYPE_TEXT_VARIATION_EMAIL_ADDRESS);
+
+            //Adds content to AlertDialog
+            LinearLayout relativeLayout = new LinearLayout(this);
+            relativeLayout.setOrientation(LinearLayout.VERTICAL);
+            relativeLayout.addView(textView);
+            relativeLayout.addView(input);
+            alertDialog.setView(relativeLayout);
+
+            //Creates OnClickListener for the Dialog message
+            DialogInterface.OnClickListener dialogOnClickListener = new DialogInterface.OnClickListener(){
+                @Override
+                public void onClick(DialogInterface dialog, int button) {
+                    switch(button){
+                        case AlertDialog.BUTTON_POSITIVE:
+                            String emailAddress = input.getText().toString();
+                            if(emailAddress.length() > 0){
+                                toggleProgressBarVisibility(View.VISIBLE);
+
+                                //Sends the email to the user if the email address is valid
+                                firebaseAuth.sendPasswordResetEmail(emailAddress).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                    @Override
+                                    public void onComplete(@NonNull Task<Void> task) {
+                                        if (task.isSuccessful()) {
+                                            //Displays message telling the user the email has been sent successfully
+                                            Toast.makeText(getApplicationContext(), "Email sent", Toast.LENGTH_LONG).show();
+                                        }
+                                        else{
+                                            String exceptionMessage = task.getException().toString();
+
+                                            //Displays appropriate error messages based on the exception message details
+                                            if(exceptionMessage.contains("FirebaseAuthInvalidUserException")){
+                                                Toast.makeText(getApplicationContext(), "There is no account associated with that email address, please enter the email address you used to create an account for this app", Toast.LENGTH_LONG).show();
+                                            }
+                                            else if(exceptionMessage.contains("FirebaseAuthInvalidCredentialsException")){
+                                                Toast.makeText(getApplicationContext(), "The email you entered is invalid, please ensure that the email address you enter exists", Toast.LENGTH_LONG).show();
+                                            }
+                                        }
+                                        toggleProgressBarVisibility(View.INVISIBLE);
+                                    }
+                                });
+                            }
+                            else{
+                                //Asks the user to enter a valid email address
+                                displayInputMessage("Please enter your email address");
+                            }
+                            break;
+                    }
+                }
+            };
+
+            //Assigns button and OnClickListener for the AlertDialog and displays the AlertDialog
+            alertDialog.setButton(AlertDialog.BUTTON_POSITIVE, "OK", dialogOnClickListener);
+            alertDialog.setCanceledOnTouchOutside(false);
+            alertDialog.show();
         }
         catch(Exception exc){
             Toast.makeText(getApplicationContext(), exc.getMessage(), Toast.LENGTH_LONG).show();
