2742cb8c560f9b0be9b58fa11af0cfdfb34fa086
matthewsyren <matthew.syren@gmail.com>
2018-07-03 16:36:56 +0200

    Added UserAccountUtilities

---
+++ SeriesSearcher/app/src/main/java/com/matthewsyren/seriessearcher/utilities/UserAccountUtilities.java
@@ -0,0 +1,66 @@
+package com.matthewsyren.seriessearcher.utilities;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.ResultReceiver;
+import android.preference.PreferenceManager;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.matthewsyren.seriessearcher.services.FirebaseService;
+
+public class UserAccountUtilities {
+    private static final String USER_KEY = "user_key";
+
+    /**
+     * Returns the user's unique key (which is stored in SharedPreferences)
+     */
+    public static String getUserKey(Context context){
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context);
+        return sharedPreferences.getString(USER_KEY, null);
+    }
+
+    /**
+     * Saves the user's unique key in SharedPreferences
+     * @param key The user's unique key for Firebase
+     */
+    public static void setUserKey(Context context, String key){
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context);
+        SharedPreferences.Editor sharedPreferencesEditor = sharedPreferences.edit();
+        sharedPreferencesEditor.putString(USER_KEY, key);
+        sharedPreferencesEditor.apply();
+    }
+
+    /**
+     * Requests the user's unique key from the Firebase Database
+     * @param resultReceiver The ResultReceiver that will process the result returned from the Service
+     */
+    public static void requestUserKey(Context context, ResultReceiver resultReceiver){
+        String emailAddress = getUserEmailAddress();
+        Intent intent = new Intent(context, FirebaseService.class);
+        intent.setAction(FirebaseService.ACTION_GET_USER_KEY);
+        intent.putExtra(Intent.EXTRA_EMAIL, emailAddress);
+        intent.putExtra(FirebaseService.RESULT_RECEIVER, resultReceiver);
+        context.startService(intent);
+    }
+
+    /**
+     * Returns the user's email address
+     */
+    public static String getUserEmailAddress(){
+        FirebaseUser firebaseUser = FirebaseAuth.getInstance()
+                .getCurrentUser();
+
+        if(firebaseUser != null){
+            return firebaseUser.getEmail();
+        }
+        return null;
+    }
+
+    //Method returns the value which states whether the user has chosen Data Saving Mode
+    public static boolean getDataSavingPreference(Context context){
+        SharedPreferences sharedPreferences = context.getSharedPreferences("", Context.MODE_PRIVATE);
+        return sharedPreferences.getBoolean("dataSavingMode", false);
+    }
+}
