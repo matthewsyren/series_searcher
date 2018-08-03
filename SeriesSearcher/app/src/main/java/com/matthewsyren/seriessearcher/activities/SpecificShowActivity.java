63bf274a73269771e6e6d3e350fb8fa437e67f64
matthewsyren <matthew.syren@gmail.com>
2018-08-03 08:46:15 +0200

    Improved the landscape layout for SpecificShowActivity

--- SeriesSearcher/app/src/main/java/com/matthewsyren/seriessearcher/activities/SpecificShowActivity.java
+++ SeriesSearcher/app/src/main/java/com/matthewsyren/seriessearcher/activities/SpecificShowActivity.java
@@ -1,12 +1,15 @@
 package com.matthewsyren.seriessearcher.activities;
 
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.content.res.Resources;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.design.widget.AppBarLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
@@ -42,12 +45,15 @@ public class SpecificShowActivity
     @BindView(R.id.text_show_runtime) TextView mTextShowRuntime;
     @BindView(R.id.text_show_rating) TextView mTextShowRating;
     @BindView(R.id.text_show_summary) TextView mTextShowSummary;
+    @Nullable
     @BindView(R.id.app_bar) AppBarLayout mAppBar;
+    @Nullable
     @BindView(R.id.toolbar) Toolbar mToolbar;
     @BindView(R.id.button_search_by_episode) FloatingActionButton mButtonSearchByEpisode;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        setTheme();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_specific_show);
         ButterKnife.bind(this);
@@ -62,12 +68,24 @@ public class SpecificShowActivity
         super.onResume();
 
         //Displays the ProgressBar if the show's information hasn't been fetched yet
-        if(mToolbar.getTitle() == null){
+        if(mToolbar != null && mToolbar.getTitle() == null){
             mProgressBar.setVisibility(View.VISIBLE);
         }
         else{
             mProgressBar.setVisibility(View.GONE);
         }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if(id == android.R.id.home){
+            onBackPressed();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
@@ -80,48 +98,72 @@ public class SpecificShowActivity
 
     //Displays a back arrow icon and adds functionality to the icon
     private void setUpBackArrowIcon(){
-        //Displays a back arrow icon
-        mToolbar.setNavigationIcon(R.drawable.ic_arrow_back_shadow);
-        mToolbar.setNavigationOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                onBackPressed();
-            }
-        });
+        if(mToolbar != null){
+            //Displays a back arrow icon
+            mToolbar.setNavigationIcon(R.drawable.ic_arrow_back_shadow);
+            mToolbar.setNavigationOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    onBackPressed();
+                }
+            });
 
-        /*
-         * Adds OffsetChangedListener to the AppBarLayout, which will display the appropriate icons when the AppBar is collapsed or expanded
-         * Adapted from https://stackoverflow.com/questions/31682310/android-collapsingtoolbarlayout-collapse-listener?utm_medium=organic&utm_source=google_rich_qa&utm_campaign=google_rich_qa
-         */
-        mAppBar.addOnOffsetChangedListener(new AppBarLayout.OnOffsetChangedListener() {
-            @Override
-            public void onOffsetChanged(AppBarLayout appBarLayout, int verticalOffset) {
-                if (Math.abs(verticalOffset) - appBarLayout.getTotalScrollRange() == 0) {
-                    //Displays a back Button icon with no shadow when the AppBar is collapsed
-                    mToolbar.setNavigationIcon(R.drawable.ic_arrow_back_white_24dp);
-                }
-                else {
-                    //Displays a back Button icon with a shadow when the AppBar is expanded (in order to make the icon visible regardless of the image behind it)
-                    mToolbar.setNavigationIcon(R.drawable.ic_arrow_back_shadow);
-                }
+            if(mAppBar != null){
+                /*
+                 * Adds OffsetChangedListener to the AppBarLayout, which will display the appropriate icons when the AppBar is collapsed or expanded
+                 * Adapted from https://stackoverflow.com/questions/31682310/android-collapsingtoolbarlayout-collapse-listener?utm_medium=organic&utm_source=google_rich_qa&utm_campaign=google_rich_qa
+                 */
+                mAppBar.addOnOffsetChangedListener(new AppBarLayout.OnOffsetChangedListener() {
+                    @Override
+                    public void onOffsetChanged(AppBarLayout appBarLayout, int verticalOffset) {
+                        if (Math.abs(verticalOffset) - appBarLayout.getTotalScrollRange() == 0) {
+                            //Displays a back Button icon with no shadow when the AppBar is collapsed
+                            mToolbar.setNavigationIcon(R.drawable.ic_arrow_back_white_24dp);
+                        }
+                        else {
+                            //Displays a back Button icon with a shadow when the AppBar is expanded (in order to make the icon visible regardless of the image behind it)
+                            mToolbar.setNavigationIcon(R.drawable.ic_arrow_back_shadow);
+                        }
+                    }
+                });
             }
-        });
+        }
+    }
+
+    //Sets the appropriate theme based on the device's orientation
+    private void setTheme(){
+        int orientation = getResources().getConfiguration().orientation;
+
+        if(orientation == Configuration.ORIENTATION_LANDSCAPE){
+            setTheme(R.style.AppTheme);
+
+            //Displays back button
+            if(getSupportActionBar() != null){
+                getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            }
+        }
+        else{
+            setTheme(R.style.AppTheme_NoActionBar);
+        }
     }
 
     //Method displays the information on the Activity
     public void displayShowInformation(){
         //Fetches the link for the show that the user clicked on from the Bundle
         Bundle bundle = getIntent().getExtras();
-        String showNumber = bundle.getString("showNumber");
-        String showLink = "http://api.tvmaze.com/shows/" + showNumber;
 
-        //Displays ProgressBar
-        toggleProgressBar(View.VISIBLE);
+        if(bundle != null){
+            String showNumber = bundle.getString("showNumber");
+            String showLink = "http://api.tvmaze.com/shows/" + showNumber;
 
-        //Fetches data from the TVMaze API using the link
-        APIConnection api = new APIConnection();
-        api.delegate = this;
-        api.execute(showLink);
+            //Displays ProgressBar
+            toggleProgressBar(View.VISIBLE);
+
+            //Fetches data from the TVMaze API using the link
+            APIConnection api = new APIConnection();
+            api.delegate = this;
+            api.execute(showLink);
+        }
     }
 
     //Method toggles the visibility of the ProgressBar
@@ -223,7 +265,16 @@ public class SpecificShowActivity
 
             //Displays the JSON data in the GUI components
             Resources resources = this.getResources();
-            mToolbar.setTitle(name);
+
+            if(mToolbar != null){
+                mToolbar.setTitle(name);
+            }
+            else{
+                if(getSupportActionBar() != null){
+                    getSupportActionBar().setTitle(name);
+                }
+            }
+
             mTextShowPremiered.setText(resources.getString(R.string.text_premiered, premiered));
             mTextShowLanguage.setText(resources.getString(R.string.text_language, language));
             mTextShowStatus.setText(resources.getString(R.string.text_status, status));
@@ -295,19 +346,27 @@ public class SpecificShowActivity
         //Fetches the show name
         String showName = null;
 
-        if(mToolbar.getTitle() != null){
+        if(mToolbar != null && mToolbar.getTitle() != null){
             showName = mToolbar.getTitle().toString();
+        }
+        else{
+            if(getSupportActionBar() != null && getSupportActionBar().getTitle() != null){
+                showName = getSupportActionBar().getTitle().toString();
+            }
         }
 
         //Fetches the link for the show that the user clicked on from the Bundle
         Bundle bundle = getIntent().getExtras();
-        String showNumber = bundle.getString("showNumber");
-        String previousActivity = bundle.getString("previousActivity");
 
-        Intent intent = new Intent(SpecificShowActivity.this, SearchByEpisodeActivity.class);
-        intent.putExtra("showTitle", showName);
-        intent.putExtra("showNumber", showNumber);
-        intent.putExtra("previousActivity", previousActivity);
-        startActivity(intent);
+        if(bundle != null){
+            String showNumber = bundle.getString("showNumber");
+            String previousActivity = bundle.getString("previousActivity");
+
+            Intent intent = new Intent(SpecificShowActivity.this, SearchByEpisodeActivity.class);
+            intent.putExtra("showTitle", showName);
+            intent.putExtra("showNumber", showNumber);
+            intent.putExtra("previousActivity", previousActivity);
+            startActivity(intent);
+        }
     }
 }
