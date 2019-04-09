06301346806c618ce28e19b7bc555c76a9fd9d4f
matthewsyren <matthew.syren@gmail.com>
2019-04-09 14:58:16 +0200

    Added the AsyncTaskUtilities class

---
+++ SeriesSearcher/app/src/main/java/com/matthewsyren/seriessearcher/utilities/AsyncTaskUtilities.java
@@ -0,0 +1,25 @@
+package com.matthewsyren.seriessearcher.utilities;
+
+import android.os.AsyncTask;
+
+public class AsyncTaskUtilities {
+    /**
+     * Cancels the AsyncTask
+     * @param asyncTask The AsyncTask to be cancelled
+     */
+    public static void cancelAsyncTask(AsyncTask asyncTask){
+        //Cancels the AsyncTask if it is still running
+        if(isAsyncTaskRunning(asyncTask) && !asyncTask.isCancelled()){
+            asyncTask.cancel(true);
+        }
+    }
+
+    /**
+     * Determines if the AsyncTask is running
+     * @param asyncTask The AsyncTask to be checked
+     * @return True if the AsyncTask is running, otherwise returns false
+     */
+    public static boolean isAsyncTaskRunning(AsyncTask asyncTask){
+        return asyncTask != null && asyncTask.getStatus() == AsyncTask.Status.RUNNING;
+    }
+}
