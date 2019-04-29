0d82ae62a14aa648894f19c5243a3a4984a52181
matthewsyren <matthew.syren@gmail.com>
2019-04-29 12:57:31 +0200

    Added a method to the DeviceUtilities class

--- SeriesSearcher/app/src/main/java/com/matthewsyren/seriessearcher/utilities/DeviceUtilities.java
+++ SeriesSearcher/app/src/main/java/com/matthewsyren/seriessearcher/utilities/DeviceUtilities.java
@@ -1,7 +1,9 @@
 package com.matthewsyren.seriessearcher.utilities;
 
 import android.content.Context;
+import android.util.DisplayMetrics;
 import android.view.Window;
+import android.view.WindowManager;
 import android.view.inputmethod.InputMethodManager;
 
 public class DeviceUtilities {
@@ -18,5 +20,15 @@ public class DeviceUtilities {
         if(inputMethodManager != null){
             inputMethodManager.hideSoftInputFromWindow(window.getDecorView().getWindowToken(), 0);
         }
+    }
+
+    /**
+     * Returns the device's height in pixels
+     * @param windowManager The WindowManager of the calling Activity (retrieved using the getWindowManager() method)
+     */
+    public static int getDeviceHeight(WindowManager windowManager){
+        DisplayMetrics displayMetrics = new DisplayMetrics();
+        windowManager.getDefaultDisplay().getMetrics(displayMetrics);
+        return displayMetrics.heightPixels;
     }
 }
